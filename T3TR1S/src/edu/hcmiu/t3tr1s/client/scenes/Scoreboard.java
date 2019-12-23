f66f15870fbcdb0cfe0b3f2f7db8c5dc68b32a8c
Doki <minhdiep1280@gmail.com>
2019-12-23 12:21:57 +0700

    Updated Scoreboard

--- T3TR1S/src/edu/hcmiu/t3tr1s/client/scenes/Scoreboard.java
+++ T3TR1S/src/edu/hcmiu/t3tr1s/client/scenes/Scoreboard.java
@@ -1,12 +1,17 @@
 package edu.hcmiu.t3tr1s.client.scenes;
 
 import edu.hcmiu.t3tr1s.client.Client;
+import edu.hcmiu.t3tr1s.client.buttons.Button;
+import edu.hcmiu.t3tr1s.core.Input;
 import edu.hcmiu.t3tr1s.core.Renderer;
 import edu.hcmiu.t3tr1s.graphics.Rectangle;
 import edu.hcmiu.t3tr1s.utils.FileUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
+
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ENTER;
 
 /**
  * The scoreboard scene for displaying past scores and high scores.
@@ -14,19 +19,23 @@ import java.util.Scanner;
  */
 
 public class Scoreboard extends Scene{
-    private static ArrayList<String> playerScores;
+    private Button button;
+
+    private static ArrayList<Integer> playerScores;
 
     Scoreboard(String name, Rectangle background) {
         super(name, background);
+        initScoreList();
     }
 
-    private static void loadData(ArrayList<String> playerScores) {
+    private static void loadData(ArrayList<Integer> playerScores) {
         String dat = FileUtils.loadAsString("data/client/score.dat");
         Scanner scanner = new Scanner(dat);
         while (scanner.hasNextLine()) {
             String score = scanner.nextLine();
-            playerScores.add(score);
+            playerScores.add(Integer.parseInt(score));
         }
+        Collections.sort(playerScores, Collections.reverseOrder());
     }
 
     static void initScoreList() {
@@ -34,18 +43,25 @@ public class Scoreboard extends Scene{
         loadData(playerScores);
     }
 
+    private void handleSelection(Client client) {
+        button.action(client);
+    }
+
     @Override
     public void show(Renderer renderer) {
-
+        background.show(renderer);
+        button.show(renderer);
     }
 
     @Override
     public void hide(Renderer renderer) {
-
+        background.hide(renderer);
+        button.hide(renderer);
     }
 
     @Override
     public void update(Client client) {
-
+        if (Input.isKeyPress(GLFW_KEY_ENTER))
+            handleSelection(client);
     }
 }
