250a0d0acae480b694e590465f0ac872224d860d
Doki <minhdiep1280@gmail.com>
2019-12-27 14:44:24 +0700

    To show scores

--- T3TR1S/src/edu/hcmiu/t3tr1s/client/scenes/Scoreboard.java
+++ T3TR1S/src/edu/hcmiu/t3tr1s/client/scenes/Scoreboard.java
@@ -4,7 +4,9 @@ import edu.hcmiu.t3tr1s.client.Client;
 import edu.hcmiu.t3tr1s.client.buttons.Button;
 import edu.hcmiu.t3tr1s.core.Input;
 import edu.hcmiu.t3tr1s.core.Renderer;
+import edu.hcmiu.t3tr1s.core.ShaderManager;
 import edu.hcmiu.t3tr1s.graphics.Rectangle;
+import edu.hcmiu.t3tr1s.math.Vector3f;
 import edu.hcmiu.t3tr1s.utils.FileUtils;
 
 import java.io.BufferedWriter;
@@ -24,11 +26,67 @@ import static org.lwjgl.glfw.GLFW.GLFW_KEY_ENTER;
 public class Scoreboard extends Scene {
     private Button button;
 
+    private ArrayList<ArrayList<Rectangle>> showScoresOnScreen;
+
     private static ArrayList<Integer> playerScores;
 
     Scoreboard(String name, Rectangle background) {
         super(name, background);
         initScoreList();
+    }
+
+    private Rectangle addDigit(int score, Vector3f topLeft, float width, float height, ShaderManager shaderManager) {
+        String textureName = "";
+
+        switch (score % 10) {
+            case 1:
+                textureName = "DIGIT_ONE";
+                break;
+            case 2:
+                textureName = "DIGIT_TWO";
+                break;
+            case 3:
+                textureName = "DIGIT_THREE";
+                break;
+            case 4:
+                textureName = "DIGIT_FOUR";
+                break;
+            case 5:
+                textureName = "DIGIT_FIVE";
+                break;
+            case 6:
+                textureName = "DIGIT_SIX";
+                break;
+            case 7:
+                textureName = "DIGIT_SEVEN";
+                break;
+            case 8:
+                textureName = "DIGIT_EIGHT";
+                break;
+            case 9:
+                textureName = "DIGIT_NINE";
+                break;
+            case 0:
+                textureName = "DIGIT_ZERO";
+                break;
+        }
+
+        return (new Rectangle(topLeft, width, height, "REGULAR_RECTANGLE", textureName, shaderManager));
+    }
+
+    private void initScoreboard(ShaderManager shaderManager) {
+        showScoresOnScreen = new ArrayList<>();
+
+        Vector3f topLeft = new Vector3f(10.0f, 1.0f, 0.5f);
+        float width = 36.0f;
+        float height = 12.0f;
+
+        playerScores.forEach(score -> {
+            showScoresOnScreen.add(new ArrayList<>());
+            for (int i = score; i != 0; i /= 10) {
+                showScoresOnScreen.get(playerScores.indexOf(score)).add(addDigit(score, topLeft, width, height, shaderManager));
+            }
+        });
     }
 
     private static void loadData(ArrayList<Integer> playerScores) {
