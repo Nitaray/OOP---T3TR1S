f5a01f765ed817eef8ee9775d1648d89b16e2187
An <justhoaian00@gmail.com>
2019-12-29 11:48:41 +0700

    Implemented ShapeRandomizer

--- T3TR1S/src/edu/hcmiu/t3tr1s/client/logic/ShapeRandomizer.java
+++ T3TR1S/src/edu/hcmiu/t3tr1s/client/logic/ShapeRandomizer.java
@@ -1,8 +1,10 @@
 package edu.hcmiu.t3tr1s.client.logic;
 
-import edu.hcmiu.t3tr1s.blocks.logic.LogicShape;
+import edu.hcmiu.t3tr1s.blocks.*;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * A random shape generator for the game logic.
@@ -12,49 +14,68 @@ import java.util.ArrayList;
  */
 
 class ShapeRandomizer {
-    private ArrayList<LogicShape> bag;
-    private int length;
 
-    ShapeRandomizer(ArrayList<LogicShape> bag, int length) {
+    private static int length;
+    private ArrayList<Shape> bag;
+    private static int randomNum;
+
+    /**
+    public static void setLength(int length) {
+        ShapeRandomizer.length = length;
+    }
+     */
+
+    ShapeRandomizer(ArrayList<Shape> bag, int length) {
         this.bag = bag;
         this.length = length;
     }
 
-    /**
-     * Generate the next random permutation if the current one is empty.
-     * Should do nothing if the current permutation has not been used up yet.
-     */
-
-    void randomize() {
-
+    private static int getRandomNumberInRange(int length) {
+        Random r = new Random();
+        return r.nextInt((7 - 1) + 1) + 1;
     }
 
-    /**
-     * Get the next shape in the current permutation.
-     * Delete that shape out of the current permutation.
-     * @return
-     */
 
-//    Shape getNext() {
-//        Shape next;
-//        return next;
-//    }
+    public void shapeRandomizer() {
+        Shape[] shapes = new Shape[7];
+        List<Integer> listOfRandomInt = new ArrayList<>();
 
-    /**
-     * Add another shape to the bag.
-     * @param logicShape the shape to be removed.
-     */
+        for (int i = 0; i < length; i++) {
+            if (i == 0 || i >= 7) {
+                randomNum = getRandomNumberInRange(7);
+                listOfRandomInt.add(randomNum);
+            }
+            if (i > 0 && i < 7) {
+                randomNum = getRandomNumberInRange(7);
+                while (listOfRandomInt.contains(randomNum)) {
+                    randomNum = getRandomNumberInRange(7);
+                }
+                listOfRandomInt.add(randomNum);
+            }
+        }
 
-    void addShape(LogicShape logicShape) {
+        shapes[0] = new IShape();
+        shapes[1] = new JShape();
+        shapes[2] = new LShape();
+        shapes[3] = new OShape();
+        shapes[4] = new SShape();
+        shapes[5] = new TShape();
+        shapes[6] = new ZShape();
 
+        for (int j = 0; j < length; j++) {
+            bag.add(shapes[listOfRandomInt.get(j)]);
+        }
     }
 
-    /**
-     * Remove a shape from the bag.
-     * @param logicShape the shape to be removed.
-     */
-
-    void removeShape(LogicShape logicShape) {
-
+    public void remove(Shape input) {
+        for (int j = 0; j < bag.size(); ++j)
+        {
+            if (bag.get(j) == input) {
+                bag.remove(j);
+                break;
+            } else {
+                continue;
+            }
+        }
     }
 }
